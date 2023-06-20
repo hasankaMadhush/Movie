0fd0f57403082f85a5c6db37b58fa12dfacc5942
Hasanka <hmadush@gmail.com>
2023-06-21 00:42:28 +0200

    User resource update

--- api/src/resources/collection/collection.interface.ts
+++ api/src/resources/collection/collection.interface.ts
@@ -1,9 +1,9 @@
 import { Document, Types } from 'mongoose';
 
-interface MovieCollection extends Document {
+interface Collection extends Document {
   name: string;
   owner: Types.ObjectId;
   movies: [Types.ObjectId];
 }
 
-export default MovieCollection;
+export default Collection;
--- api/src/resources/user/user.validation.ts
+++ api/src/resources/user/user.validation.ts
@@ -1,14 +1,14 @@
 import Joi from 'joi';
 
-const register = Joi.object({
+const create = Joi.object({
   name: Joi.string().max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
 
-const login = Joi.object({
+const authenticate = Joi.object({
   email: Joi.string().required(),
   password: Joi.string().required(),
 });
 
-export default { register, login };
+export default { create, authenticate };
