ec7a09ca727ddfff452e2f0d190dcc738723a464
Hasanka <hmadush@gmail.com>
2023-06-20 23:46:03 +0200

    token utils method name change

--- api/src/utils/token.ts
+++ api/src/utils/token.ts
@@ -3,13 +3,13 @@ import jwt from 'jsonwebtoken';
 import User from 'resources/user/user.interface';
 import Token from 'utils/interfaces/token.interface';
 
-const createToken = (user: User): string => {
+const create = (user: User): string => {
   return jwt.sign({ id: user._id }, process.env.JWT_SECRET as jwt.Secret, {
     expiresIn: '1d',
   });
 };
 
-const verifyToken = async (token: string): Promise<jwt.VerifyErrors | Token> => {
+const verify = async (token: string): Promise<jwt.VerifyErrors | Token> => {
   return new Promise((resolve, reject) => {
     jwt.verify(token, process.env.JWT_SECRET as jwt.Secret, (error, payload) => {
       if (error) {
@@ -20,4 +20,4 @@ const verifyToken = async (token: string): Promise<jwt.VerifyErrors | Token> => 
   });
 };
 
-export default { createToken, verifyToken };
+export default { create, verify };
