a9e66f025085c3a278a6a045ca74608bf2f2265d
Hasanka <hmadush@gmail.com>
2023-06-21 00:53:42 +0200

    refactor movieCollection to Collection in user.service

--- api/src/resources/user/user.service.ts
+++ api/src/resources/user/user.service.ts
@@ -1,14 +1,14 @@
 import { Model } from 'mongoose';
 
 import Collection from 'resources/collection/collection.interface';
-import MovieCollectionModel from 'resources/collection/collection.model';
+import CollectionModel from 'resources/collection/collection.model';
 import token from 'utils/token';
 import User from './user.interface';
 import UserModel from 'resources/user/user.model';
 
 class UserService {
   private user: Model<User> = UserModel;
-  private movieCollection: Model<Collection> = MovieCollectionModel;
+  private collection: Model<Collection> = CollectionModel;
 
   public async create(
     name: string,
@@ -46,23 +46,23 @@ class UserService {
   }
   public async getCollections(userId: string): Promise<Collection[] | Error> {
     try {
-      return await this.movieCollection
+      return await this.collection
         .find({ owner: userId })
         .populate({ path: 'owner', select: '_id name' })
         .populate({ path: 'movies' });
     } catch (error) {
-      throw new Error('Unable to get movie collection');
+      throw new Error('Unable to get collection');
     }
   }
 
   public async getOtherCollections(userId: string): Promise<Collection[] | Error> {
     try {
-      return await this.movieCollection
+      return await this.collection
         .find({ owner: { $ne: userId } })
         .populate({ path: 'owner', select: '_id name' })
         .populate({ path: 'movies' });
     } catch (error) {
-      throw new Error('Unable to get movie collection');
+      throw new Error('Unable to get collection');
     }
   }
 }
