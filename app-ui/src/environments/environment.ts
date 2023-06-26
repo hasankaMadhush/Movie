c70186adda50c2c9676c71fa6baa1960e4eec5f2
Hasanka <hmadush@gmail.com>
2023-06-27 00:05:16 +0200

    update prod env

--- app-ui/src/environments/environment.ts
+++ app-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  server: 'http://localhost:4000/api',
+  server: 'https://movie-api-hzil.onrender.com/api',
   defaultLimit: 50,
   defaultOffset: 1,
   defaultDashboardCollectionLimit: 5,
