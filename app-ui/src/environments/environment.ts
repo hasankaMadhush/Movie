7c451fa36db8b79a0ad4b8b7ab462284d2a085ea
Hasanka Madhushan <hmadush@gmail.com>
2023-06-26 15:05:49 -0700

    adding env back (#25)

---
+++ app-ui/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  server: 'https://movie-api-hzil.onrender.com/api',
+  defaultLimit: 50,
+  defaultOffset: 1,
+  defaultDashboardCollectionLimit: 5,
+  defaultDashboardMovieLimit: 10,
+  defualtDateFormat: 'DD/MM/YYYY',
+};
