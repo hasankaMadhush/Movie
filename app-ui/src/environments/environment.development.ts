b7c5a646b2509f5812ee7841faad60c221647acc
Hasanka Madhushan <hmadush@gmail.com>
2023-06-26 14:31:53 -0700

    Remove env fe (#23)

--- app-ui/src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  server: 'http://localhost:4000/api',
-  defaultLimit: 50,
-  defaultOffset: 1,
-  defaultDashboardCollectionLimit: 5,
-  defaultDashboardMovieLimit: 10,
-  defualtDateFormat: 'DD/MM/YYYY',
-};
